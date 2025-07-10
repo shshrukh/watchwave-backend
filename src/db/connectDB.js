578e5bd068adaa4a794c864def51af977d9eac91
shshrukh <shshrukh.arbaz@gmail.com>
2025-07-11 00:12:29 +0500

    usning express implement some functions app.on and app.lesten

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -6,6 +6,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`Mongoes connected, connection Host: ${connectionInstance.connection.host}`);
+        return connectionInstance;
         
     } catch (error) {
         console.log("ERROR: failed to connect DB,",error);
@@ -13,4 +14,4 @@ const connectDB = async () => {
     }
 }
 
-export default connectDB;``
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import { app } from "./app.js";
 
 dotenv.config({path:'./.env'});
 
+// console.log(process);
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on('error', err => {
+            console.log("EXPRESS error", err); 
+            throw err;
+        });
+
+        app.listen(process.env.PORT || 4000, () => {
+            console.log("server is running on port 3000");
+            
+        })
+    })
+    .catch(err => console.log('MONGOOES db connect faild !!!', err))
+    
