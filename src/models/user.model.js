25bc5c571a2b8e2faa90dfa4ccc71ac81aed0f04
shshrukh <shshrukh.arbaz@gmail.com>
2025-07-14 17:28:17 +0500

    update user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,54 +1,92 @@
-import mongoose ,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
-const userSchema =new Schema({
-    username : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true,
-        index : true
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true,
-        lowercase : true,
-        trim : true
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     fullname: {
-        type : String,
-        required : true,
-        index : true,
-        trim : true
+        type: String,
+        required: true,
+        index: true,
+        trim: true
     },
-    avatar : {
-        type : String,
-        required : true,
+    avatar: {
+        type: String,
+        required: true,
     },
-    coverimage : {
-        type : String,
-        required : true
-    }, 
-    watchHistory : [
+    coverimage: {
+        type: String,
+        required: true
+    },
+    watchHistory: [
         {
-            type : mongoose.Types.ObjectId,
-            ref : "Video"
+            type: mongoose.Types.ObjectId,
+            ref: "Video"
         }
     ],
-    passowrd : {
-        type : String,
-        required : [true, "password is required"]
+    passowrd: {
+        type: String,
+        required: [true, "password is required"]
     },
-    refreshToken : {
-        type : String
+    refreshToken: {
+        type: String
     }
-    
-},
-{
-    timestamps : true
-}
-)
 
+},
+    {
+        timestamps: true
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.passowrd = bcrypt.hash(this.passowrd, 10);
+    next()
+})
+userSchema.methods.isPasswordCorrect = async function (passowrd) {
+    return await bcrypt.compare(passowrd, this.passowrd)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRT
+        }
+    )
+};
 export const User = mongoose.model("User", userSchema);
 
