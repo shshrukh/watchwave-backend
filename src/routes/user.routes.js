2170fe53787fbca9519a587ea687b080fdf49c7e
shshrukh <shshrukh.arbaz@gmail.com>
2025-08-28 14:35:37 +0500

    feat: add change password route with JWT verification

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser} from "../controllers/user.controller.js";
+import { registerUser,loginUser, logoutUser, changePassword} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlerware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js" 
 
@@ -25,6 +25,8 @@ router.route("/login").post( loginUser);
 //secure user
 
 router.route("/logout").post( verifyJWT, logoutUser )
+
+router.route("/change-password").post( verifyJWT, changePassword)
 
 
 export default router
